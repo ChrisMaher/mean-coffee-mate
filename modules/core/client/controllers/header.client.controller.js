bb5558ef11c8579c17e5142e405c4efddec43aa8
Chris Maher <chrismaher.wit@gmail.com>
2016-03-31 17:08:45 +0100

    Mobile Redirects.

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -2,6 +2,48 @@
 
 angular.module('core').controller('HeaderController', ['$scope', '$location', '$state', 'Authentication', 'Menus',
   function ($scope, $location, $state, Authentication, Menus) {
+
+    var isMobile = {
+      Android: function() {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+
+    };
+
+    $scope.isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+
+    if ( isMobile.Android() ) {
+      document.location.href = "https://play.google.com/store/apps/details?id=coffeemate.chris.app.coffeemateclub";
+    }
+    else if(isMobile.iOS())
+    {
+      document.location.href = "http://www.saveme.ie/apple";
+    }
+    else if(isMobile.BlackBerry())
+    {
+      document.location.href = "http://www.saveme.ie/android";
+    }else if(isMobile.Windows())
+    {
+      document.location.href = "https://www.microsoft.com/en-gb/store/apps/coffeemate/9nblggh4m5b1";
+    }
+
+
     // Expose view variables
     $scope.$state = $state;
     $scope.authentication = Authentication;
