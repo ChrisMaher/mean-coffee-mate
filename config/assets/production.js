0f270ec1fac1b3c6dfc38a8e18685c5bb2015f65
Chris Maher <chrismaher.wit@gmail.com>
2016-04-16 21:59:47 +0100

    View Button.

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,19 +6,42 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/angular-material/angular-material.min.css',
+        'http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-aria/angular-aria.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-material/angular-material.min.js',
+        'public/lib/angular/angular-route.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angulike/angulike.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
         'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/angular-utils-pagination/dirPagination.js',
+
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js'
+        'public/lib/textAngular/dist/textAngular-rangy.min.js',
+        'public/lib/textAngular/dist/textAngular-sanitize.min.js',
+        'public/lib/textAngular/dist/textAngularSetup.js',
+
+        'public/lib/textAngular/dist/textAngular.min.js',
+        'public/lib/angular-wysiwyg/dist/angular-wysiwyg.min.js',
+        'public/lib/angular-wysiwyg/dist/bootstrap-colorpicker-module.js',
+
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/moment/min/moment.min.js',
+        'public/lib/angular-moment/angular-moment.min.js'
+
+
+
       ]
     },
     css: 'public/dist/application.min.css',
     js: 'public/dist/application.min.js'
   }
 };
+
